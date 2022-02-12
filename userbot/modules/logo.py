abb706b1051fac870f8d4a1175190ec046a652cd
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-02-12 11:52:59 +0700

    Fixed!

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -17,8 +17,8 @@ async def _(event):
     if not text:
         await event.edit("`Give a name too!`")
     else:
-        await event.edit("`Processing`")
-    chat = "@JokerXRobot"
+        await event.edit("`Processing...`")
+    chat = "@tdtapibot"
     async with event.client.conversation(chat) as conv:
         try:
             msg = await conv.send_message(f"/logo {text}")
@@ -28,7 +28,7 @@ async def _(event):
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit(
-                "**Error: Mohon Buka Blokir** `@JokerXRobot` **Dan Coba Lagi!**"
+                "**Error : Mohon Buka Blokir** `@tdtapibot` **Dan Coba Lagi!**"
             )
             return
         await asyncio.sleep(0.5)
